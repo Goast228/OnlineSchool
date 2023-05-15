2b56a8f431912ddef1f278a49470440b392c8e83
arseniy <samoylov6566@gmail.com>
2023-05-15 22:45:47 +0300

    First-Last Commit

---
+++ LearnScool/Models/Enities/ClientTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnScool.Models.Enities;
+
+public partial class ClientTag
+{
+    public int ClientTagId { get; set; }
+
+    public string TagName { get; set; } = null!;
+
+    public string TagHashColor { get; set; } = null!;
+
+    public virtual ICollection<Client> Clients { get; set; } = new List<Client>();
+}
---
+++ LearnScool/Models/Enities/ServiceImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnScool.Models.Enities;
+
+public partial class ServiceImage
+{
+    public int ServiceImageId { get; set; }
+
+    public int ServiceId { get; set; }
+
+    public byte[] Image { get; set; } = null!;
+
+    public virtual Service Service { get; set; } = null!;
+}
